a5a7c9afc7955b956b43ad1137260a8596fd6cee
victorjunior95 <victorjr.muchacho@gmail.com>
2021-10-03 15:51:14 -0300

    Removing ToDo task - task 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,6 +25,9 @@ export function Home() {
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
